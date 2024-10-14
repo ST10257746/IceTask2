3d644ebadcc083e6158367afa6215a41282029f4
ST10257746 <ST10257746@vcconnect.edu.za>
2024-10-14 09:59:22 +0200

    Ice 2

---
+++ st10257746_ice2_CLDV6212/IceTask2_ST10257746/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using IceTask2_ST10257746.Models;
+
+namespace IceTask2_ST10257746.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ st10257746_ice2_CLDV6212/IceTask2_ST10257746/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IceTask2_ST10257746.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
